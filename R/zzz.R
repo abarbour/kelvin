3ff244415ea778a691e389e15d02f42de8a1539a
Andy Barbour <andy.barbour@gmail.com>
2013-03-12 09:43:59 -0700

    Last.lib caused CRAN check note. removed.

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,9 @@
     sprintf("Loaded kelvin (%s) -- Solutions to the Kelvin differential equation.",
             utils:::packageVersion("kelvin")))
 }
-.Last.lib <- function(...){
-  NULL
-}
+# CRAN check (3.0.0):
+# * checking R code for possible problems ... NOTE
+# NB: .Last.lib will not be used unless it is exported
+# .Last.lib <- function(...){
+#   NULL
+# }
